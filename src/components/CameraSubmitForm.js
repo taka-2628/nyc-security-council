d18fb7c80c5eac9625cde4cbcfbacda6691c4e28
Takashi <th2826@columbia.edu>
2022-09-19 15:22:32 -0400

    CameraSubmitForm component created

---
+++ src/components/CameraSubmitForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+function CameraSubmitForm( { /*currentUser*/ } ){
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [errors, setErrors] = useState([]);
+
+  const navigate = useNavigate();
+  
+  function handleSubmit(e){
+    e.preventDefault();  
+    setErrors([]);
+    
+    /*
+    fetch("/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: currentUser.id,
+        title,
+        description,
+        image,
+        genres: selectedGenres,
+        technologies: selectedTechs
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((project) => {
+          setProjects([...projects, project])
+          navigate("/");
+        });
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+    */
+  }
+
+  return (
+    <div id="submit-camera">
+      <h1>Submit Surveillance Camera</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="half-width div-left">
+          <label htmlFor="p-title">Title</label>
+          <input 
+            type="text" 
+            id="p-title" 
+            name="title"
+            value={title}
+            placeholder="Title of your project.." 
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="half-width div-right">
+          <label htmlFor="p-image">Image</label>
+          <input 
+            type="text" 
+            id="p-image" 
+            name="image"
+            value={image}
+            placeholder="URL for an image.."
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </div>
+        
+        <label htmlFor="p-desc">Camera Description</label>
+        <textarea 
+          id="c-desc" 
+          name="description"
+          value={description}
+          placeholder="Describe the surveillance camera.."
+          onChange={(e) => setDescription(e.target.value)}
+        >
+        </textarea>
+      
+        <input type="submit" value="Submit"/>
+      </form>
+      <div className="error-div">
+        {errors.map((err) => (
+            <p key={err} >{err}</p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CameraSubmitForm;
--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -1,10 +1,12 @@
 import NavBar from "./NavBar";
+import CameraSubmitForm from "./CameraSubmitForm";
 
 function Contribute(){
   return(
     <div id="contribute" className="width-60">
       <h1>Contribute</h1>
       <NavBar />
+      <CameraSubmitForm />
     </div>
   )
 }
