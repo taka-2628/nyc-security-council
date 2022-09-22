00b9f15e3b30926011797df8d4ce1b83cc081d91
Takashi <th2826@columbia.edu>
2022-09-22 17:08:58 -0400

    Home component and its styling updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,13 @@ function Home(){
   return(
     <div id="home">
       <NavFullScreen />
+      <div id="dark-filter"></div>
+      <div id="title-animation-cont">
+        <div id="title-animation-wrapper">
+          <h1>New York City Security Council</h1>
+          <h1 className="title-second">New York City Security Council</h1>
+        </div>
+      </div>
     </div>
   )
 }
